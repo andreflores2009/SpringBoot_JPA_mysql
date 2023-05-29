9d4539b8aeb29d9aa4a1005f55ebb03199575e3f
André F S <58055547+andreflores2009@users.noreply.github.com>
2023-05-29 18:31:10 -0300

    Add files via upload

---
+++ jpa_spring/src/test/java/com/spring/jpa_spring/JpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
